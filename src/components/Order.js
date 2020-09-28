207a0888281723e11613e2eac5c5811052f75651
Haiyang Hong <hong961127@gmail.com>
2020-09-27 17:24:57 -0700

    Explore variants, transition orchestration, children transition staggering

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,14 +1,51 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    x: "100vw",
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      mass: 0.4, // Higher mass moves slower
+      damping: 8, // Damping force: 0 -> damps indefintely
+      when: "beforeChildren", // Transition : ochestration transition happens before children's transition
+      staggerChildren: 0.4, // Stagger each children's transition by 0.4s
+    },
+  },
+};
+
+const childVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
 
 const Order = ({ pizza }) => {
   return (
-    <div className="container order">
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className="container order"
+    >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map((topping) => (
-        <div key={topping}>{topping}</div>
-      ))}
-    </div>
+      <motion.p variants={childVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childVariants}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
+    </motion.div>
   );
 };
 
